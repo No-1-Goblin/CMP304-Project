ec8625c29cc72585a1631c4305f4904a1ef939af
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2024-03-22 15:53:05 +0000

    Add comments

--- CMP 304 Project/Assets/Scripts/Brain.cs
+++ CMP 304 Project/Assets/Scripts/Brain.cs
@@ -16,9 +16,12 @@ public class Brain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Setup event handler listeners
         eventHandler.awardPoints.AddListener(AssignReward);
         eventHandler.killPlayer.AddListener(RemoveRandomness);
+        // Get player movement component
         playerMovement = GetComponent<PlayerMovement>();
+        // Reset brain
         ResetBrain();
     }
 
@@ -30,26 +33,35 @@ public class Brain : MonoBehaviour
 
     public void ResetBrain()
     {
+        // Reset Q matrix
         Q = new float[hazardTypes, actionTypes];
         oddsToRandomise = 20;
     }
 
     public void AlertBrain(ApproachingHazards newHazard)
     {
+        // Update memory of last hazard
         lastHazard = newHazard;
+        // Determine action to take
         actionTaken = DetermineAction(newHazard);
+        // Perform determined action
         PerformAction(actionTaken);
     }
 
     public void AssignReward(bool positive)
     {
+        // Determine whether to reward a positive reward or negative punishment
         float reward = positive ? positiveReward : negativeReward;
-        Q[(int)lastHazard, (int)actionTaken] = Q[(int)lastHazard, (int)actionTaken] + learningRate * (reward + discountFactor * GetBestAction(lastHazard).Item2 - Q[(int)lastHazard, (int)actionTaken]);
+        // Apply changes to Q matrix
+        Q[(int)lastHazard, (int)actionTaken] = Q[(int)lastHazard, (int)actionTaken] + learningRate * 
+        (reward + discountFactor * GetBestAction(lastHazard).Item2 - Q[(int)lastHazard, (int)actionTaken]);
     }
 
     Actions DetermineAction(ApproachingHazards hazard)
     {
+        // Calculate best action based on Q matrix
         Actions actionToPerform = GetBestAction(hazard).Item1;
+        // Random chance to perform random action instead
         if (UnityEngine.Random.Range(1, 101) <= oddsToRandomise)
         {
             actionToPerform = (Actions)UnityEngine.Random.Range(0, actionTypes);
@@ -60,15 +72,21 @@ public class Brain : MonoBehaviour
 
     Tuple<Actions, float> GetBestAction(ApproachingHazards hazard)
     {
+        // Create variable to hold best action found so far
         Tuple<Actions, float> bestAction = new((Actions)0, Q[(int)hazard, 0]);
+        // Iterate through all actions for currently approaching hazard in Q matrix
         for (int i = 0; i < actionTypes; i++)
         {
+            // If the action currently being checked has a greater expected reward than the previously best found action
             if (Q[(int)hazard, i] > bestAction.Item2)
             {
+                // Update best action
                 bestAction = new((Actions)i, Q[(int)hazard, i]);
             }
+            // If the action has an equal expected reward to the previously best found action
             else if (Q[(int)hazard, i] == bestAction.Item2)
             {
+                // Choose a random action out of the two
                 bestAction = UnityEngine.Random.Range(0, 2) == 0 ? bestAction : new((Actions)i, Q[(int)hazard, i]);
             }
         }
@@ -89,6 +107,7 @@ public class Brain : MonoBehaviour
                 playerMovement.Stand();
                 break;
         }
+        // Keep record of last action taken
         actionTaken = actionToTake;
         Debug.Log("Approaching hazard was a: " + lastHazard);
         Debug.Log("I chose to " + actionTaken);
